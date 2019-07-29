5d69db564b2e63b95aea3c854dd9ca1d06c284a3
mvaughn1234 <m.vaughn1234@gmail.com>
2019-07-29 16:57:51 -0400

    redoing generator in python ...

--- scripts/py_scripts/runGenerateConfigs.py
+++ scripts/py_scripts/runGenerateConfigs.py
@@ -14,7 +14,7 @@ configs = None;
 procCount = 1;
 
 def createProps(configJSON):
-    print('configs: ', configJSON.len)
+    print('configs: ', len(configJSON))
     print('configs: ', configJSON[0])
     print('configs: ', configJSON[0]['configs'])
     print('configs: ', configJSON[0]['configs'][0])
