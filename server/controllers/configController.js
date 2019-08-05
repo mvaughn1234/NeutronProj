fc8d850cf6f8329ba011cda28b80ca41ae91fda2
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-05 14:41:48 -0400

    porting functionality to local environment for running server on carbon

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -56,55 +56,6 @@ exports.deleteConfig = (req, res) => {
             res.status(500).send(err)
         });
 };
-//
-// .then(config => {
-//     console.log('made it here');
-//     console.log('config mode: ', config.mode);
-//     if (config.mode === 'single') {
-//         console.log('made it here2');
-//         const matList = `-mats=${config.matList.map(mat => mat.name).join(',')}`;
-//         const lenList = `-lens=${config.lenList.map(len => {
-//             return len.single ? len.min : `${len.min}-${len.max}:${len.part}`;
-//         }).join(',')}`;
-//         console.log('made it here3');
-//         Setting.find().then(data => data[0]).then(settings => {
-//             console.log('made it here4');
-//             console.log('Settings: ', settings.settings.filter(element => element.title === 'Energy Min')[0].currentValue);
-//             const eLow = Number(settings.settings.filter(element => element.title === 'Energy Min')[0].currentValue) * (10 ** -3);
-//             const eHigh = Number(settings.settings.filter(element => element.title === 'Energy Max')[0].currentValue);
-//             const numBins = Number(settings.settings.filter(element => element.title === 'Num Bins')[0].currentValue);
-//             const scale = settings.settings.filter(element => element.title === 'Set Scale')[0].currentValue;
-//             console.log('made it here5');
-//             const energy = `-En=${eLow}-${eHigh}:${numBins}@${scale}`;
-//             const flags = config.flags.join(',');
-//             console.log('made it here6');
-//             try {
-//                 let runPhrase = `ruby ${rb_gen_path_local} ${matList} ${lenList} ${energy} ${flags}`;
-//                 console.log(runPhrase);
-//                 // const genData = spawn(`sshpass -p ${carbonPass} ssh ${carbonUser}@carbon444.umm.edu ruby ${rb_gen_path}`, [matList, lenList, energy, flags]);
-//                 const genData = spawn(`ruby ${rb_gen_path_local}`, [matList, lenList, energy, flags]);
-//                 genData.stdout.on('data', (data) => {
-//                     console.log(`stdout: ${data}`);
-//                 });
-//
-//                 genData.stderr.on('data', (data) => {
-//                     console.log(`stderr: ${data}`);
-//                 });
-//
-//                 genData.on('close', (code) => {
-//                     console.log(`child process exited with code ${code}`);
-//                 });
-//
-//                 genData.on('error', (err) => {
-//                     console.log(`failed to start data generation with error ${err}`);
-//                 });
-//             } catch (err) {
-//                 console.log(err);
-//             }
-//         })
-//             .then(res.status(200).send('running configs'));
-//     }
-// })
 
 let exportToJsonFile = (jsonData) => {
     let dataStr = JSON.stringify(jsonData);
@@ -253,6 +204,17 @@ exports.runConfigs = (io, Configs) => {
             });
 
             const generator = spawn('python3', ['/home/student/geant4/NeutronProj/scripts/py_scripts/runGenerateConfigs.py', fullPath])
+            generator.stdout.on('data', (data) => {
+                console.log(`stdout: ${data}`)
+            });
+
+            generator.stderr.on('data', (data) => {
+                console.log(`stderr: ${data}`)
+            });
+
+            generator.on('close', (code) => {
+                console.log(`generator exited with code ${code}`)
+            })
 
 
         }));
