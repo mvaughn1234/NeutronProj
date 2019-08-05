b40b7392938e80b0b1d389be77c47a7383faf058
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-05 17:35:16 -0400

    modifying add dataset controller to function both for starting a new mat table and updating one. (minor fix)

--- server/controllers/matDBController.js
+++ server/controllers/matDBController.js
@@ -1,50 +1,73 @@
 const MatDB = require('../models/MatDB');
 const Mat = require('../models/Mat');
 
-exports.getMatDBList = (req,res) => {
+exports.getMatDBList = (req, res) => {
     MatDB.find()
         .then(matDBs => res.json(matDBs));
 };
 
-exports.getMatDB = (req,res) => {
+exports.getMatDB = (req, res) => {
     MatDB.find({'mat.name': req.params.name})
         .then(matDB => res.json(matDB))
         .catch(err => res.status(500).send(err));
 };
 
-exports.addDataSet = (req,res) => {
+exports.addDataSet = (req, res) => {
     MatDB.find({'mat.name': req.params.name, 'data.len': req.params.length})
         .then(matDB => {
             console.log('matDB', matDB);
-            if (matDB.length === 0){
-                Mat.find({"name": req.params.name})
-                    .then(mat => {
-                        if (mat.length === 0){
-                            res.status(500).send("Couldn't find material")
+            if (matDB.length === 0) {
+                MatDB.find({'mat.name': req.params.name})
+                    .then(matDB2 => {
+                        if (matDB2.length === 0) {
+                            Mat.find({"name": req.params.name})
+                                .then(mat => {
+                                    if (mat.length === 0) {
+                                        res.status(500).send("Couldn't find material")
+                                    } else {
+                                        const lenSet = {len: req.params.length, lenSet: [req.body]};
+                                        const newDB = {mat: mat[0], data: [lenSet]};
+                                        const newMatDB = new MatDB(newDB);
+                                        newMatDB.save();
+                                        const msg = 'Adding new mat database: ' + req.params.name;
+                                        console.log(msg);
+                                        res.status(201).send(msg)
+                                    }
+                                })
                         }else{
                             const lenSet = {len: req.params.length, lenSet: [req.body]};
-                            const newDB = {mat: mat[0], data: [lenSet]};
-                            const newMatDB = new MatDB(newDB);
-                            newMatDB.save();
-                            res.status(201).send(newMatDB);
+                            MatDB.update({'mat.name': req.params.name},
+                                {$push: {'data': lenSet}})
+                                .then(matDB => {
+                                    const msg = 'Adding new len set to ' + req.params.name + ' database';
+                                    console.log(msg);
+                                    res.status(200).send(msg)
+                                })
+                                .catch(err => res.status(500).send(err))
                         }
                     })
-            }else{
+            } else {
                 console.log('here');
                 MatDB.update({'mat.name': req.params.name, 'data.len': req.params.length},
                     {$push: {'data.$.lenSet': req.body}})
-                    .then(matDB => {console.log('here2'); console.log('matDB', matDB); res.json(matDB)})
+                    .then(matDB => {
+                        const msg = 'Adding new data set to ' + req.params.name + ' lenSet at length ' + req.params.length;
+                        console.log(msg);
+                        res.status(200).send(msg)
+                    })
                     .catch(err => res.status(500).send(err))
             }
         })
 };
 
-exports.deleteMatDB = (req,res) => {
+exports.deleteMatDB = (req, res) => {
     MatDB.find({'mat.name': req.params.name, 'data.length': req.params.length})
         .then(matDB => {
             matDB.data.length(req.params.length).remove();
             matDB.save()
-                .then(() => {res.status(204).send('removed');})
+                .then(() => {
+                    res.status(204).send('removed');
+                })
         })
         .catch(err => res.status(500).send(err));
 };
