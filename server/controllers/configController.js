05ee1983d0e6e8d63d3ef749d2b969d70daf5649
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-07 18:51:22 -0400

    test pwd

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -201,7 +201,8 @@ exports.runConfigs = (socket, Configs) => {
                 })
             });;//a
 
-            const generator = spawn('. /home/ubuntu/Downloads/geant/build/geant4make.sh && python3', ['/home/student/geant4/NeutronProj/scripts/py_scripts/runGenerateConfigs.py', fullPath], {shell: true});
+            // const generator = spawn('. /home/ubuntu/Downloads/geant/build/geant4make.sh && python3 && pwd', ['/home/student/geant4/NeutronProj/scripts/py_scripts/runGenerateConfigs.py', fullPath], {shell: true});
+            const generator = spawn('pwd');
             generator.stdout.on('data', (stdout) => {
                 socket.emit('runConfigsClient', 'stdout: ' + stdout+'\n');
                 console.log(`stdout: ${stdout}`)
