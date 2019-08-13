51677dec56e0177803eb789524e163f7de081cb6
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-13 13:30:38 -0400

    expanding limit

--- server/app.js
+++ server/app.js
@@ -40,12 +40,12 @@ app.set('view engine', 'pug');
 
 // Middleware
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser());
+app.use(bodyParser({limit: '50mb'}));
 
 // Socket.io Generate Data
 const generator = http.Server(app);
