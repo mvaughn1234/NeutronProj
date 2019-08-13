e71fad6549670c7c12e6caab59f5f8b2fb321697
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-13 13:11:28 -0400

    putting urlencoded and bodyparser back in

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser);
+app.use(bodyParser());
 
 // Socket.io Generate Data
 const generator = http.Server(app);
