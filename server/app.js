b237197762e9a204c5aaf21899bcf3ef7f0359a2
mvaughn1234 <m.vaughn1234@gmail.com>
2019-08-13 13:06:44 -0400

    putting urlencoded and bodyparser back in

--- server/app.js
+++ server/app.js
@@ -41,15 +41,15 @@ app.set('view engine', 'pug');
 // Middleware
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.json({
-//   parameterLimit: 100000,
-//   limit: '50mb',
-//   extended: true
-// }));
+app.use(bodyParser.json({
+  parameterLimit: 100000,
+  limit: '50mb',
+  extended: true
+}));
 
 // Socket.io Generate Data
 const generator = http.Server(app);
